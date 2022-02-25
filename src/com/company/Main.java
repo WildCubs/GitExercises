545257f2791fd373aceb2856be0ab3a6c0371f73
WildCubs <phil379c@stud.kea.dk>
2022-02-25 13:24:38 +0100

    added askAboutAge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,11 +10,16 @@ Scanner keybdINput = new Scanner(System.in);
         String inputName = keybdINput.next();
         System.out.println("Good morning, " + inputName);
     }
-
+    public void askAboutAge() {
+        System.out.println("What is your age?");
+        int inputAge = keybdINput.nextInt();
+        System.out.println(inputAge + " years old? Or young, depending on one's own scope.");
+    }
 
     public static void main(String[] args) {
         System.out.println("Workflow works!");
         Main getMe = new Main();
         getMe.greet();
+        getMe.askAboutAge();
     }
 }
