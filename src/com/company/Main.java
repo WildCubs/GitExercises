a84d2cf72c382f450b0dfd596d3da69ad4e1f32e
WildCubs <phil379c@stud.kea.dk>
2022-02-25 13:16:34 +0100

    added Greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+Scanner keybdINput = new Scanner(System.in);
+
+    public void greet() {
+        System.out.println("Hello. What is your name?");
+        String inputName = keybdINput.next();
+        System.out.println("Good morning, " + inputName);
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Workflow works!");
+        Main getMe = new Main();
+        getMe.greet();
     }
 }
